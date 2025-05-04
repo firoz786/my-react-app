0820eae5e6cfdf1eb84eea933f16e8ead1d9a324
firoz alam <firozalam786123@gmail.com>
2025-05-04 14:13:59 +0000

    .

---
+++ myapp/src/Test.js
@@ -0,0 +1,13 @@
+export function Test() {
+    console.log("firoz");
+    let obj  = {
+        fname : "firoz",
+        lname : "alam",
+        addr : {
+            street:"no7",
+            state:"bihar"
+        }
+    }
+    console.log("obj", obj)
+    return null;
+}
